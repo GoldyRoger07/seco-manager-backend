4d4709ca5993fbeb010f8fb76856d8e8e2b38c68
GoldyRoger07 <latouche822@gmail.com>
2026-01-16 10:58:36 -0500

    upupupnomi

--- src/badges/badges.controller.ts
+++ src/badges/badges.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, MulterModule } from '@nestjs/platform-express';
 import { BadgesService } from './badges.service';
+import { join } from 'node:path';
+import { createReadStream } from 'node:fs';
 
 @Controller('/api/badges')
 export class BadgesController {
@@ -16,5 +18,23 @@ export class BadgesController {
     ) {
         return this.badgesService.generateBadge(body, file);
     }
+
+
+
+    // @Get('download/:filename')
+    // downloadImage(@Param('filename') filename: string, @Res() res: Response) {
+    //     const filePath = join(process.cwd(), 'output', filename);
+
+    //     // res.download(filePath);
+    //     const file = createReadStream(filePath);
+        
+    //     return new StreamableFile(file, {
+    //         type: 'image/png',
+    //         disposition: `attachment; filename="${filename}"`,
+    //         // If you want to define the Content-Length value to another value instead of file's length:
+    //         // length: 123,
+    //     });
+    // }
+
 
 }
--- src/badges/badges.service.ts
+++ src/badges/badges.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { createCanvas, loadImage, registerFont } from 'canvas';
+import { createCanvas, Image, loadImage, registerFont } from 'canvas';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -34,15 +34,60 @@ export class BadgesService {
         console.log(file)
         const photo = await loadImage(file.buffer);
         console.log(photo)
-        ctx.drawImage(photo, 668, 186, 300, 300);
+        // ctx.drawImage(photo, 668, 186, 300, 300);
+        this.drawRoundedImage(ctx, photo, 668, 186, 300, 300, 15)
 
         
         const outputPath = path.join(process.cwd(), 'output', `${data.id}.png`);
         fs.writeFileSync(outputPath, canvas.toBuffer('image/png'));
-        const relativeOutputPath = `/files/output/${data.id}.png`
+        const relativeOutputPath = `http://localhost:3000/files/output/${data.id}.png`
         console.log(relativeOutputPath)
-        // return { file: relativeOutputPath };
-        return canvas.toBuffer('image/png');
+        return { file: relativeOutputPath };
+        // return canvas.toBuffer('image/png');
     }
+
+    drawRoundedImage(ctx: any, img: any, x:number, y:number, width:number, height:number, radius:number) {
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height - radius);
+      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+
+      ctx.clip(); // applique le masque
+
+      this.drawImageCover(ctx,img,x,y,width,height)
+      // ctx.drawImage(img, x, y, width, height);
+    }
+
+    drawImageCover(ctx: CanvasRenderingContext2D, img:any, x:number, y:number, w:number, h:number) {
+      const imgRatio = img.width / img.height;
+      const boxRatio = w / h;
+
+      let drawWidth, drawHeight, offsetX, offsetY;
+
+      if (imgRatio > boxRatio) {
+        // Image plus large → on coupe sur les côtés
+        drawHeight = h;
+        drawWidth = h * imgRatio;
+        offsetX = (w - drawWidth) / 2;
+        offsetY = 0;
+      } else {
+        // Image plus haute → on coupe en haut/bas
+        drawWidth = w;
+        drawHeight = w / imgRatio;
+        offsetX = 0;
+        offsetY = (h - drawHeight) / 2;
+      }
+
+      ctx.drawImage(img, x + offsetX, y + offsetY, drawWidth, drawHeight);
+    }
+
+
 
 }
